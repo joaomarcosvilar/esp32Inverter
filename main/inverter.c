b14fdc30109cd082796a070717b4f9a5851a6de5
João Marcos Vilar <joaomarcossbj172001@gmail.com>
2025-11-04 18:12:52 -0300

    adc read and change duty cycle

--- main/inverter.c
+++ main/inverter.c
@@ -4,6 +4,7 @@
 
 #include "driver/mcpwm_prelude.h"
 #include "driver/gpio.h"
+#include "esp_adc/adc_continuous.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -14,13 +15,23 @@
 #define PWM_B_PIN                       GPIO_NUM_4
 
 #define MCPWM_RESOLUTION                (1e7) // 100ns por tick
-#define MCPWM_PERIOD_TICKS              (100) // 10us
+#define MCPWM_PERIOD_TICKS              (25) // 2,5us
 #define MCPWM_TIMER_COUNT               MCPWM_TIMER_COUNT_MODE_UP
 #define MCPWM_DUTY_TICKS                MCPWM_PERIOD_TICKS / 2
-#define MCPWM_DEAD_TICKS                (10) // 1us
+#define MCPWM_DEAD_TICKS                (5) // 0,5us
+
+#define ADC_CHANNEL                     ADC_CHANNEL_6
+#define ADC_UNIT                        ADC_UNIT_1
+#define ADC_CONV_MODE                   ADC_CONV_SINGLE_UNIT_1
+#define ADC_ATTEN                       ADC_ATTEN_DB_12
+#define ADC_BIT_WIDTH                   SOC_ADC_DIGI_MAX_BITWIDTH
+#define ADC_READ_LEN                    (64)
+#define ADC_BUFFER_SIZE                 (512)
+#define ADC_SAMPLE_FREQ_HZ              (1e6)
+#define ADC_OUTPUT_TYPE                 ADC_DIGI_OUTPUT_FORMAT_TYPE1
 
 #define TASK_NAME                       "control task"
-#define TASK_STACK_SIZE                 1024 * 1
+#define TASK_STACK_SIZE                 1024 * 3
 #define TASK_PRIOR                      2
 
 static mcpwm_timer_handle_t timer;
@@ -28,15 +39,28 @@ static mcpwm_oper_handle_t operator_a, operator_b;
 static mcpwm_cmpr_handle_t comparator_a_high, comparator_a_low; 
 static mcpwm_cmpr_handle_t comparator_b_high, comparator_b_low;
 static mcpwm_gen_handle_t generator_a, generator_b;
+static adc_continuous_handle_t adc_handle;
+static TaskHandle_t controller_task_handle = NULL; 
+
+static bool IRAM_ATTR adc_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+{
+    BaseType_t mustYield = pdFALSE;
+
+    if(controller_task_handle != NULL)
+    {
+        vTaskNotifyGiveFromISR(controller_task_handle, &mustYield);
+    }
+
+    return (mustYield == pdTRUE);
+}
 
 esp_err_t mcpwm_set_duty(uint8_t percent)  // Esperado valores de 0 - 100
 {
     if(percent > 100)
         percent = 100;
     
-    uint32_t ticks =  ((MCPWM_DUTY_TICKS - MCPWM_DEAD_TICKS) * (percent / 100.0f))+ MCPWM_DEAD_TICKS;
+    uint32_t ticks =  ((MCPWM_DUTY_TICKS - MCPWM_DEAD_TICKS) * (percent / 100.0f)) + MCPWM_DEAD_TICKS;
 
-    ESP_LOGI(TAG, "Set ticks: %d + %d + %ld\n\n",MCPWM_DUTY_TICKS, MCPWM_DEAD_TICKS, ticks);
     esp_err_t ret = mcpwm_comparator_set_compare_value(comparator_a_low, ticks);
     if(ret != ESP_OK)
     {
@@ -106,8 +130,20 @@ esp_err_t mcpwm_init(void)
     {
         return ret;
     }
+    
+    ret = mcpwm_comparator_set_compare_value(comparator_a_high, (MCPWM_DEAD_TICKS));
+    if (ret != ESP_OK)
+    {
+        return ret;
+    }
 
     ret = mcpwm_new_comparator(operator_a, &comparator_config, &comparator_a_low);
+    if (ret != ESP_OK)
+    {
+        return ret;
+    }
+
+    ret = mcpwm_comparator_set_compare_value(comparator_a_low, (MCPWM_DUTY_TICKS));
     if (ret != ESP_OK)
     {
         return ret;
@@ -197,12 +233,113 @@ esp_err_t mcpwm_init(void)
     return ret;
 }
 
+esp_err_t adc_init(void)
+{
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = ADC_BUFFER_SIZE,
+        .conv_frame_size = ADC_READ_LEN
+    };
+
+    esp_err_t ret = adc_continuous_new_handle(&adc_config, &adc_handle);
+    if(ret != ESP_OK)
+    {
+        return ret;
+    }
+
+    adc_continuous_config_t dig_confg = {
+        .sample_freq_hz = ADC_SAMPLE_FREQ_HZ,
+        .conv_mode = ADC_CONV_MODE,
+        .format = ADC_OUTPUT_TYPE
+    };
+
+    adc_digi_pattern_config_t adc_parttern[SOC_ADC_PATT_LEN_MAX] = {0};
+    dig_confg.pattern_num = 1;
+    adc_parttern[0] = (adc_digi_pattern_config_t){
+        .atten = ADC_ATTEN,
+        .channel = ADC_CHANNEL & 0x7,
+        .unit = ADC_UNIT,
+        .bit_width = ADC_BIT_WIDTH
+    };
+    dig_confg.adc_pattern = adc_parttern;
+
+    ret = adc_continuous_config(adc_handle, &dig_confg);
+    if(ret != ESP_OK)
+    {
+        return ret;
+    }
+
+    adc_continuous_evt_cbs_t cbs = {
+        .on_conv_done = adc_conv_done_cb
+    };
+
+    ret = adc_continuous_register_event_callbacks(adc_handle, &cbs, NULL);
+    if(ret != ESP_OK)
+    {
+        return ret;
+    }
+
+    ret = adc_continuous_start(adc_handle);
+
+    return ret;
+}
+
+void controller_task(void *args)
+{
+    esp_err_t ret;
+    uint32_t ret_num = 0;
+    uint8_t result[ADC_READ_LEN] = {0};
+
+    while(true)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        ret = adc_continuous_read(adc_handle, result, ADC_READ_LEN, &ret_num, 0 );
+        if(ret == ESP_OK)
+        {
+            for(int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES)
+            {
+                adc_digi_output_data_t *p = (adc_digi_output_data_t*)&result[i];
+                
+                // Extrai os dados do ADC dependendo do formato
+                // uint32_t chan_num = p->type1.channel;
+                uint32_t data = p->type1.data;
+                
+                // Aqui você pode processar os dados
+                // Exemplo: converter para tensão, fazer média, filtrar, etc.
+                // ESP_LOGI(TAG, "Canal: %ld, Valor ADC: %ld", chan_num, data);
+                uint8_t percent = (data * 100) / 4095;
+                mcpwm_set_duty(percent);
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
 void app_main(void)
 {
     esp_err_t ret = mcpwm_init();
     if (ret != ESP_OK)
     {
         ESP_LOGE(TAG, "Failed to init timer");
+        return;
+    }
+
+    ret = adc_init();
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to init adc");
+        return;
+    }
+
+    BaseType_t xRet = xTaskCreate(  controller_task,
+                                    TASK_NAME,
+                                    TASK_STACK_SIZE,
+                                    NULL,
+                                    TASK_PRIOR,
+                                    &controller_task_handle);
+    if(xRet != pdTRUE)
+    {
+        ESP_LOGE(TAG,"Failed to create task");
         return;
     }
 }
